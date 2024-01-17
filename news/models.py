3d04c2f5d5f884994d992926275531b58539df80
alanguilhermeM <alanguilherme13@outlook.com>
2024-01-17 14:44:15 -0300

    FInalizado requisito 3

--- news/models.py
+++ news/models.py
@@ -1,4 +1,14 @@
 from django.db import models
+from django.core.exceptions import ValidationError
+
+
+def validate_min_words(value):
+    words = value.split()
+    if len(words) < 2:
+        raise ValidationError(
+            _('O título deve ter mais de uma palavra.'),
+            code='invalid_title',
+        )
 
 
 class Category(models.Model):
@@ -16,3 +26,19 @@ class User(models.Model):
 
     def __str__(self) -> str:
         return f'{self.name}'
+
+
+class News(models.Model):
+    title = models.CharField(
+        max_length=200,
+        blank=False,
+        validators=[validate_min_words]
+    )
+    content = models.TextField(blank=False)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateField(auto_now_add=True, blank=False)
+    image = models.ImageField(upload_to='images/', blank=True)
+    categories = models.ManyToManyField(Category, blank=False)
+
+    def __str__(self) -> str:
+        return self.title
