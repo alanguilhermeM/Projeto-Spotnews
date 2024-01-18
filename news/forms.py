aab2db22189cf89c369ed7d91dbcd89c5c5e5b55
alanguilhermeM <alanguilherme13@outlook.com>
2024-01-18 19:35:29 -0300

    Finalizado requisito 7

--- news/forms.py
+++ news/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from .models import Category, User
 
 
 class CategoriesForm(forms.Form):
@@ -7,4 +8,35 @@ class CategoriesForm(forms.Form):
         max_length=200,
         required=True,
         # widget=forms.TextInput(attrs={'type': 'text', 'name': 'name'})
+    )
+
+
+class NewsForm(forms.Form):
+    title = forms.CharField(label='Título')
+
+    content = forms.CharField(
+        label='Conteúdo',
+        widget=forms.Textarea(attrs={'rows': 5})
+    )
+
+    author = forms.ModelChoiceField(
+        label='Autoria',
+        queryset=User.objects.all()
+    )
+
+    created_at = forms.DateField(
+        label='Criado em',
+        widget=forms.DateInput(attrs={'type': 'date'})
+    )
+
+    image = forms.ImageField(
+        label='URL da Imagem',
+        required=False,
+        widget=forms.FileInput
+    )
+
+    categories = forms.ModelMultipleChoiceField(
+        label='Categorias',
+        queryset=Category.objects.all(),
+        widget=forms.CheckboxSelectMultiple
     )
--- news/views.py
+++ news/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from .models import News, Category
 from django.shortcuts import get_object_or_404
 from django.http import Http404
-from .forms import CategoriesForm
+from .forms import CategoriesForm, NewsForm
 
 
 # Create your views here.
@@ -32,3 +32,24 @@ def categories_form(request):
         form = CategoriesForm()
     context = {'forms': form}
     return render(request, 'categories_form.html', context)
+
+
+def news_form(request):
+    if request.method == 'POST':
+        form = NewsForm(request.POST, request.FILES)
+
+        if form.is_valid():
+            new_instance = News(
+                title=form.cleaned_data['title'],
+                content=form.cleaned_data['content'],
+                author=form.cleaned_data['author'],
+                created_at=form.cleaned_data['created_at'],
+                image=request.FILES.get('image'),
+            )
+            new_instance.save()
+            new_instance.categories.set(form.cleaned_data['categories'])
+            return redirect('home-page')
+    else:
+        form = NewsForm()
+    context = {'forms': form}
+    return render(request, 'news_form.html', context)
