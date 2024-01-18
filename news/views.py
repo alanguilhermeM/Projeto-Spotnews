7387b5f620f0340514b2f1b47a5bbff4140f6644
alanguilhermeM <alanguilherme13@outlook.com>
2024-01-18 10:02:00 -0300

    Feature news-details

--- news/views.py
+++ news/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
 from .models import News
+from django.shortcuts import get_object_or_404
+from django.http import Http404
 
 
 # Create your views here.
 def index(request):
     context = {"news": News.objects.all()}
     return render(request, 'home.html', context)
+
+
+def news_details(request, id):
+    try:
+        new = get_object_or_404(News, id=id)
+        return render(request, 'news_details.html', {'new': new})
+    except Http404:
+        return render(request, '404.html')
